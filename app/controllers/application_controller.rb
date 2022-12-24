1897e74747e89a780553637763c12798c9245823
Brian Lambert <brianlambert2408@gmail.com>
2022-12-24 15:56:52 +1100

    commit tree 5c99313d9b167d426651a8f9d3bed233ec267934

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorized
+
+  def authorized
+    render json: { errors: "Unauthorized user "}, status: :unauthorized unless session.include? :user_id
+  end
+
 end
